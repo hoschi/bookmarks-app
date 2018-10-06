51a0cac14d698e3ace462f701e41536d38506e96
Stefan Gojan <vollekannehoschi@gmail.com>
2018-10-06 17:04:48 +0200

    first query

--- src/BookmarksList.js
+++ src/BookmarksList.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
-
-/*
- *import gql from 'graphql-tag'
- *client
- *    .query({
- *        query: gql`
- *            {
- *                allBookmarks {
- *                    title
- *                    isRead
- *                }
- *            }
- *        `,
- *    })
- *    .then((result) => console.log(result))
- *
- */
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 function BookmarksList() {
     return (
-        <Paper>
-            <Typography component="div">My List</Typography>
-        </Paper>
+        <Query
+            query={gql`
+                {
+                    allBookmarks {
+                        title
+                        isRead
+                    }
+                }
+            `}
+        >
+            {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>
+                if (error) return <p>Error {error.toString()}</p>
+                const { allBookmarks } = data
+
+                return (
+                    <Paper>
+                        <Typography component="div">
+                            My List, items: {allBookmarks.length}
+                        </Typography>
+                    </Paper>
+                )
+            }}
+        </Query>
     )
 }
 
