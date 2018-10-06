eb64bf00f0f31c678e57e5e6f6de8dbac5ee340c
Stefan Gojan <vollekannehoschi@gmail.com>
2018-10-06 16:59:55 +0200

    setup Apollo

--- src/BookmarksList.js
+++ src/BookmarksList.js
@@ -2,6 +2,22 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 
+/*
+ *import gql from 'graphql-tag'
+ *client
+ *    .query({
+ *        query: gql`
+ *            {
+ *                allBookmarks {
+ *                    title
+ *                    isRead
+ *                }
+ *            }
+ *        `,
+ *    })
+ *    .then((result) => console.log(result))
+ *
+ */
 function BookmarksList() {
     return (
         <Paper>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import App from './App'
 
+const client = new ApolloClient({
+    uri: 'http://localhost:3009/graphql',
+})
 const rootElement = global.document.querySelector('#root')
 if (rootElement) {
     render(
-        <div>
+        <ApolloProvider client={client}>
             <CssBaseline />
             <App />
-        </div>,
+        </ApolloProvider>,
         rootElement
     )
 }
