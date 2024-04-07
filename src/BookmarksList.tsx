5caf1c7c5e9083893285e8ebb0ff0b434fbe6477
Stefan Gojan <vollekannehoschi@gmail.com>
2024-04-07 18:06:08 +0200

    basic gql

--- src/BookmarksList.tsx
+++ src/BookmarksList.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
+import { Typography, List, Divider, Paper } from '@mui/material'
+import { queryAllBookmarks } from './gql'
+import { useQuery } from '@apollo/client'
 
+// FIXME add type from GQL to `item` below
 export const BookmarksList: React.FunctionComponent = () => {
-    return <Button variant="contained">Hello</Button>
+    const { loading, error, data } = useQuery(queryAllBookmarks)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Query Error {error.toString()}</p>
+    const { allBookmarks } = data
+    return (
+        <Paper>
+            <Typography component="div">
+                <List disablePadding>
+                    {allBookmarks.map((item: any, i: number) => (
+                        <React.Fragment key={item.id}>
+                            {item.title}
+                            {i !== allBookmarks.length - 1 && <Divider />}
+                        </React.Fragment>
+                    ))}
+                </List>
+            </Typography>
+        </Paper>
+    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,20 @@ import '@fontsource/roboto/700.css'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './theme'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </ApolloProvider>
     </React.StrictMode>
 )
