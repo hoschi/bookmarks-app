63567ad0dc3a668b7e74df50da1cfa8c01c0f3f5
Stefan Gojan <vollekannehoschi@gmail.com>
2024-04-07 14:55:07 +0200

    delete all code, except config stuff

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from './App'
-
-test('render', function() {
-    const actual = shallow(<App />).dive()
-    expect(actual).toMatchSnapshot()
-})
--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// setup file
-const enzyme = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
-
-enzyme.configure({ adapter: new Adapter() })
